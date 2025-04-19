afad086b8762302298e601157264e48250968a00
DILNATHRK <rkdilnath@gmail.com>
2025-04-19 16:10:01 +0530

    fix: removed gorm tags from struct

--- SendEmail/model/model.go
+++ SendEmail/model/model.go
@@ -2,8 +2,8 @@ package model
 
 type EmailNotification struct {
 	EmailNotificationID uint   `json:"email_notification_id"`
-	Sender              string `json:"sender"     validate:"required"`
-	Recipient           string `json:"recipient"  validate:"required"`
+	Sender              string `json:"sender"      validate:"required"`
+	Recipient           string `json:"recipient"   validate:"required"`
 	Subject             string `json:"subject"`
 	Payload             string `json:"payload"`
 }
